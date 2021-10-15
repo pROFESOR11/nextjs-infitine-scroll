7ecf1081e9d71eadb90ddc6e82d54176295e3d73
pROFESOR11 <ali_cimen_11@hotmail.com>
2021-10-15 11:31:34 +0300

    add header component

---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,41 @@
+import AppBar from '@material-ui/core/AppBar'
+import Link from '@material-ui/core/Link'
+import { makeStyles } from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+import HideOnScroll from '@components/HideOnScroll'
+
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+    backgroundColor: theme.palette.grey[900],
+    color: theme.palette.common.white,
+  },
+  toolbar: {
+    display: 'flex',
+  },
+  link: {
+    padding: '12px 15px',
+    color: theme.palette.common.white,
+  },
+}))
+
+function Header(): JSX.Element {
+  const classes = useStyles()
+
+  return (
+    <HideOnScroll>
+      <AppBar elevation={0} className={classes.appbar}>
+        <Toolbar className={classes.toolbar} disableGutters>
+          <Link href="/">
+            <Typography variant="h6" color="textPrimary" className={classes.link}>
+              NextJs Infinite Scroll
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </HideOnScroll>
+  )
+}
+
+export default Header
