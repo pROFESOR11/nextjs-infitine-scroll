13397ff7b550b99b31cac59ff8d8263f6f96a32e
pROFESOR11 <ali_cimen_11@hotmail.com>
2021-10-15 11:44:07 +0300

    add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,23 @@
-export const Home = (): JSX.Element => <div>Placeholder</div>
+import Items from '@components/Items'
+import Layout from '@components/Layout'
+import useItems from '@hooks/useItems'
+
+export const Home = (): JSX.Element => {
+  const { data, isFetching, isFetchingNextPage, fetchNextPage } = useItems()
+
+  return (
+    <Layout>
+      <Layout.Header />
+      <Layout.Content>
+        <Items
+          items={data?.pages?.reduce((prev, cur) => prev.concat(cur.map((item) => item)), [])}
+          isFetching={isFetching || isFetchingNextPage}
+          fetchNextPage={fetchNextPage}
+        />
+      </Layout.Content>
+      <Layout.Footer />
+    </Layout>
+  )
+}
 
 export default Home
