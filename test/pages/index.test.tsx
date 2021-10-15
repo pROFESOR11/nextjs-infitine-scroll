1fe9e64ac7430470f6e73066e2bfac8b692b41ca
pROFESOR11 <ali_cimen_11@hotmail.com>
2021-10-15 11:36:43 +0300

    prettier eslint

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+
+import { Home } from '../../src/pages/index'
 import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
